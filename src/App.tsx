9af08c3ac6f0b853c1dabed8f8152a7ca05dda36
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-12-22 17:41:32 +0530

    remove video

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,17 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <video autoPlay loop id="vid"> <source src="assets/logos/bg.mp4" type="video/mp4"/>  </video>
+      {/* <video autoPlay loop id="vid">
+        <source src="assets/logos/bg.mp4" type="video/mp4" />
+      </video> */}
       <Box
         sx={{
-          padding: { lg: 2, md: 2, sm: 2 },
+          padding: { lg: 0, md: 2, sm: 2 },
           minHeight: "100vh",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          background: 'url("../public/assets/logos/bg.mp4")',
-        
+          background: customColors.outerBg,
         }}
       >
         <FormBox />
