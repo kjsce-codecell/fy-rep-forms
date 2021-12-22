40d6296738587b5e45987f3d9da0c2bc4a62d447
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-12-22 11:25:35 +0530

    add theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, ThemeProvider } from "@mui/material";
 import FormContext from "./context/FormContext";
 import FormBox from "./components/FormBox";
-import { lightTheme } from "./theme/theme";
+import { darkTheme, lightTheme } from "./theme/theme";
 
 function App() {
+  const [theme, setTheme] = useState(darkTheme);
   const formContext = React.useContext(FormContext);
+
+  const changeTheme = () => {
+    if (theme == darkTheme) {
+      setTheme(lightTheme);
+    } else {
+      setTheme(darkTheme);
+    }
+  };
 
   return (
     <ThemeProvider theme={lightTheme}>
