64e4ab592399018dc688aad9de916355e0356b53
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-12-22 10:25:34 +0530

    ✨ NEW: position preference checkbox

--- src/components/DetailsForm.tsx
+++ src/components/DetailsForm.tsx
@@ -1,4 +1,15 @@
-import { TextField, Box, Autocomplete } from "@mui/material";
+import {
+  TextField,
+  Box,
+  Autocomplete,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  FormLabel,
+} from "@mui/material";
+import { error } from "console";
 import React from "react";
 
 interface Props {}
@@ -6,6 +17,27 @@ interface Props {}
 const branches = ["COMPS", "IT", "Electronics", "EXTC", "MECH"];
 
 const DetailsForm = (props: Props) => {
+  const [state, setState] = React.useState({
+    TechnicalTeam: false,
+    PublicRelationsTeam: false,
+    CreativeTeam: false,
+    Coordinator: false,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  const { Coordinator, CreativeTeam, PublicRelationsTeam, TechnicalTeam } =
+    state;
+  const error =
+    [Coordinator, CreativeTeam, PublicRelationsTeam, TechnicalTeam].filter(
+      (v) => v
+    ).length > 3;
+
   return (
     <Box
       component="form"
@@ -28,6 +60,58 @@ const DetailsForm = (props: Props) => {
           renderInput={(params) => <TextField {...params} label="Branch" />}
         />
       </Box>
+      <FormControl
+        required
+        error={error}
+        component="fieldset"
+        sx={{ m: 3 }}
+        variant="standard"
+      >
+        <FormLabel component="legend">Position Preference</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={Coordinator}
+                onChange={handleChange}
+                name="Coordinator"
+              />
+            }
+            label="Coordinator"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={CreativeTeam}
+                onChange={handleChange}
+                name="CreativeTeam"
+              />
+            }
+            label="Creative Team"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={PublicRelationsTeam}
+                onChange={handleChange}
+                name="PublicRelationsTeam"
+              />
+            }
+            label="Public Relations Team"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={TechnicalTeam}
+                onChange={handleChange}
+                name="TechnicalTeam"
+              />
+            }
+            label="Technical Team"
+          />
+        </FormGroup>
+        <FormHelperText>At Most 3</FormHelperText>
+      </FormControl>
     </Box>
   );
 };
