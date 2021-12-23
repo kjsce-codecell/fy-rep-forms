8ab4788506e000b4bf9c24f026d1a526c44cf8f2
jaymalave <jaym.github@gmail.com>
2021-12-23 12:23:07 +0530

    fixed

--- src/components/DetailsForm.tsx
+++ src/components/DetailsForm.tsx
@@ -97,8 +97,7 @@ const DetailsForm = ({
     }
     if (
       !emailRef.current!.value.includes("@") ||
-      emailRef.current!.value.indexOf("@") === 0 ||
-      !emailRef.current!.value.includes(".com")
+      emailRef.current!.value.indexOf("@") === 0
     ) {
       setEmailError(true);
       console.log("Is this even your email?");
