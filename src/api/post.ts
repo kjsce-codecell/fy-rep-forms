0549735c6426af0bc520b399868ceb70f9a3c275
Nishit Patel <nishitlimbani130@gmail.com>
2021-12-23 17:26:04 +0530

    Add code for sending email response

--- src/api/post.ts
+++ src/api/post.ts
@@ -10,17 +10,56 @@ export const POST = async (object?: any) => {
   var linkedin = object.linkedin;
   var codechef = object.codechef;
   var q1 = object.q1;
+  var year = "FY";
   var positions = " ";
   object.positions.forEach((element: String) => {
     positions += element + ", ";
   });
+
   const url = `https://docs.google.com/forms/d/e/1FAIpQLSfc4fpHWG_LD4CUkWbUgEuIF0tobNGnK2TISAJ_bczSBMreqA/formResponse?usp=pp_url&entry.1060656156=${name}&entry.1698873613=${email}&entry.1143253914=${phone}&entry.1156550514=FY&entry.1553288753=${branch}&entry.1416789219=${positions}&entry.1089965806=${resume}&entry.809758529=${cover}&entry.887347889=${codechef}&entry.903534579=${github}&entry.1423818616=${linkedin}&entry.2064142506=${q1}&sumbit=Submit`;
+
+  let allData = {
+    email,
+    name,
+    phone,
+    year,
+    branch,
+    positions: object.positions,
+    resume,
+    cover,
+    codechef,
+    github,
+    linkedin,
+    q1: q1,
+    q2: "",
+    q3: "",
+    url,
+  };
 
   return fetch("https://cors-fix.nishit.workers.dev/?" + url, {
     method: "POST",
   }).then((res) => {
     console.log(res);
     if (res.status == 200) {
+      
+
+      fetch(
+          "https://cors-fix.nishit.workers.dev/?https://us-central1-codecell-interviews.cloudfunctions.net/sendMail",
+          {
+              method: "POST",
+              body: JSON.stringify({
+                  email: email,
+                  data: allData,
+              }),
+              headers: {
+                  "Content-Type": "application/json",
+              },
+              mode: "cors",
+              cache: "no-cache",
+          }
+      ).then (res => {
+        console.log("response: ", res.status);
+      })
       return "Yayay";
     } else {
       console.log ("res.status: ", res.status);
